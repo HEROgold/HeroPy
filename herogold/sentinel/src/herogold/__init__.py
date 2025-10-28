e88a338e97d572fd7b12ee48cd080f610020a575
HEROgold <martijnwieringa28@gmail.com>
2025-10-28 18:23:17 +0100

    move to one dir

--- herogold/sentinel/src/herogold/__init__.py
+++
@@ -1,7 +0,0 @@
-"""herogold namespace package for bundled subpackages.
-
-This file intentionally left minimal so the `herogold` package
-is available when this distribution is installed.
-"""
-
-__all__ = []
