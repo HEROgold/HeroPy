0b1a07c2435cbe19920cc29a9bd718195b29dbd5
HEROgold <martijnwieringa28@gmail.com>
2026-04-12 20:35:51 +0200

    Refactor logging setup: remove FileHandler and its references from the logging module

--- src/herogold/log/__init__.py
+++ src/herogold/log/__init__.py
@@ -3,21 +3,19 @@
 from __future__ import annotations
 
 from .formats import Formatter, formatter, message, prefix
-from .handlers import FileHandler, StreamHandler, file_handler, stream_handler
+from .handlers import StreamHandler, stream_handler
 from .logger_mixin import LoggerMixin
 from .logging import INFO, basicConfig
 
 basicConfig(
     level=INFO,
-    handlers=[stream_handler, file_handler],
+    handlers=[stream_handler],
 )
 
 __all__ = [
-    "FileHandler",
     "Formatter",
     "LoggerMixin",
     "StreamHandler",
-    "file_handler",
     "formatter",
     "message",
     "prefix",
