1b1377ca87b43d7a27c5821d6cd9b56c4deeeac3
HEROgold <martijnwieringa28@gmail.com>
2026-04-19 00:42:15 +0200

    update docstring, rename as_group to with_group, and improve type annotations

--- src/herogold/errors.py
+++ src/herogold/errors.py
@@ -1,16 +1,21 @@
-"""Module for enhancing python's error handling capabilities."""
+"""Module for enhancing python's error handling capabilities.
+
+Decorators with_exception(), with_group()
+to wrap functions and handle exceptions in a more structured way.
+"""
 
 from __future__ import annotations
 
+from functools import wraps
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from collections.abc import Callable, Iterable
+    from collections.abc import Callable, Generator, Iterable
 
 
 def with_exception[**P, T](func: Callable[P, T]) -> Callable[P, T | Exception]:
     """Wrap a function and returns any thrown exception."""
-
+    @wraps(func)
     def wrapper(*args: P.args, **kwargs: P.kwargs) -> T | Exception:
         try:
             return func(*args, **kwargs)
@@ -20,9 +25,9 @@ def with_exception[**P, T](func: Callable[P, T]) -> Callable[P, T | Exception]:
     return wrapper
 
 
-def as_group[**P, T](func: Callable[P, Iterable[T | Exception]]) -> Callable[P, Iterable[T] | ExceptionGroup]:
+def with_group[**P, T](func: Callable[P, Iterable[T | Exception]]) -> Callable[P, Iterable[T] | ExceptionGroup]:
     """Collect exceptions from an iterable of results and raise them as an ExceptionGroup."""
-
+    @wraps(func)
     def wrapper(*args: P.args, **kwargs: P.kwargs) -> Iterable[T] | ExceptionGroup:
         results = func(*args, **kwargs)
         exceptions: list[Exception] = []
@@ -49,12 +54,17 @@ if __name__ == "__main__":
     def test(i: int) -> float:  # noqa: D103
         return 10 / i
 
-    @as_group
-    def test_group():  # noqa: ANN201, D103
+    @with_group
+    def test_group() -> Generator[float | Exception]:  # noqa: D103
+        return (test(i) for i in range(-2, 3))
+
+    def test_generator() -> Generator[float | Exception]:  # noqa: D103
         return (test(i) for i in range(-2, 3))
 
     r1 = test(0)
     r2 = test_group()
+    r3 = test_generator()
 
     print(r1)  # noqa: T201
     print(r2)  # noqa: T201
+    print(list(r3))  # noqa: T201
