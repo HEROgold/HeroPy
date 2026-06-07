421ec962c2761cdc3e8c6832187e412f3feac1ca
HEROgold <martijnwieringa28@gmail.com>
2026-06-08 00:35:16 +0200

    Fix typing on loops

--- src/herogold/loops.py
+++ src/herogold/loops.py
@@ -17,14 +17,12 @@ cpu_count = os.cpu_count() or 1
 def _square(value: int) -> int:
     return value * value
 
-
-def parallel[T, **P](action: Callable[P, T], data: Iterable[T]) -> Iterator[T]:
+def parallel[T, P](action: Callable[[P], T], data: Iterable[P]) -> Iterator[T]:
     """Run a function in parallel across multiple CPU cores."""
     with ProcessPoolExecutor(max_workers=cpu_count) as executor:
         yield from executor.map(action, data, chunksize=10)
 
-
-async def a_parallel[T, **P](action: Callable[P, T], data: AsyncIterable[T]) -> AsyncIterator[T]:
+async def a_parallel[T, P](action: Callable[[P], T], data: AsyncIterable[P]) -> AsyncIterator[T]:
     """Run a function in parallel across multiple CPU cores from async code."""
     loop = get_async_loop()
 
