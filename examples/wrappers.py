43ebf2605b75c3ce5b42a43bbbd662b4d5e1dc23
HEROgold <m21345384+HEROgold@users.noreply.github.com>
2026-09-09 21:58:10 +0200

    Add helper types for decorator and decorator factory

--- examples/wrappers.py
+++ examples/wrappers.py
@@ -6,8 +6,11 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from collections.abc import Callable
 
+type Decorator[**P, R] = Callable[P, R]
+type DecoratorFactory[**P, R] = Callable[[Callable[P, R]], Decorator[P, R]]
 
-def wrapper[F, **P](func: Callable[P, F]) -> Callable[P, F]:
+
+def wrapper[F, **P](func: Callable[P, F]) -> Decorator[P, F]:
     @wraps(func)
     def inner(*args: P.args, **kwargs: P.kwargs) -> F:
         return func(*args, **kwargs)
@@ -15,8 +18,8 @@ def wrapper[F, **P](func: Callable[P, F]) -> Callable[P, F]:
     return inner
 
 
-def decorator_factory[**P, F](*args: P.args, **kwargs: P.kwargs) -> Callable[[Callable[P, F]], Callable[P, F]]:
-    def wrapper[F](func: Callable[P, F]) -> Callable[P, F]:
+def decorator_factory[**P, F](*args: P.args, **kwargs: P.kwargs) -> DecoratorFactory[P, F]:
+    def wrapper(func: Decorator[P, F]) -> Decorator[P, F]:
         @wraps(func)
         def inner(*_: P.args, **__: P.kwargs) -> F:
             return func(*args, **kwargs)
